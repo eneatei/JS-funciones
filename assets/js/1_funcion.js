d602a4376d59076c2d73313ae51656696f4c46af
eneatei <nataliaochoa@MacBook-Pro-de-Natalia.local>
2023-09-09 22:31:38 -0300

    desafios ok

---
+++ assets/js/1_funcion.js
@@ -0,0 +1,7 @@
+//! Función de expresión
+const example = function (a, b , c) {
+    let result = console.log(a + b + c);
+    return result;
+}
+
+example(20, 30, 50);
---
+++ assets/js/2_arrow.js
@@ -0,0 +1,4 @@
+//! Función en una sola linea
+const suma = (a,b) => a+b;
+
+suma(20, 25);
